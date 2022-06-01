da7e92affa8b1dce5d6bbcae8381c2c72cb2a530
Dmitriy Lyner <DmitriyLyner@gmail.com>
2022-06-01 14:22:35 -0600

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'springboard-embeddables';
+  searchStore = (window as any).getSearchStore();
+
+  async query(event: any) {
+    const detail = event.detail;
+    console.log(detail);
+
+    // Example demonstrating the public search store
+    const queryResponse = await this.searchStore.search({ query: detail.query })
+    console.log('Query response', queryResponse);
+
+  }
 }
